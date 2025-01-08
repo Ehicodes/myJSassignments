e412d77bc794e633ebf9d7e92dcb1d71f1b85a09
Ehicodes <osaigbovoehinomen@gmail.com>
2025-01-08 22:23:12 +0100

    third assignment

--- assignments.js
+++ assignments.js
@@ -1,15 +1,15 @@
 /*ASSIGNMENT 1 (VALUES AND VARIABLES)  Declare variables called 'country', 'continent' and 'population' and
 assign their values according to your own country (population in millions) Log their values to the console */
 
-let country = "Nigeria";
+// let country = "Nigeria";
 
-let continent = "Africa";
+// let continent = "Africa";
 
-let population = 2238000000;
+// let population = 2238000000;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
 /* ASSIGNMENT 2 /*Declare a variable called 'isIsland' and set its value according to your
 country. The variable should hold a Boolean value. Also declare a variable
@@ -17,11 +17,23 @@ country. The variable should hold a Boolean value. Also declare a variable
  Log the types of 'isIsland', 'population', 'country' and 'language'
 to the console  */
 
-let isIsland = true;
+// let isIsland = true;
 
-let language;
+// let language;
 
-console.log(typeof isIsland);
-console.log(typeof language);
-console.log(typeof country);
-console.log(typeof population);
+// console.log(typeof isIsland);
+// console.log(typeof language);
+// console.log(typeof country);
+// console.log(typeof population);
+
+/*ASSIGNMENT 3 (let, const and var)Set the value of 'language' to the language spoken where you live (some countries have multiple languages, but just choose one)Think about which variables should be const variables (whichvalues will never change, and which might change?). Then, change these variables to const. Try to change one of the changed variables now, and observe what happens */
+
+const language = "Yoruba";
+
+const country = "Nigeria";
+
+const continent = "Africa";
+
+const population = 2238000000;
+
+population = 22000000; //WILL NOT CHANGE INSTEAD WILL FLAG AN ERROR BECAUSE CONST IS A KEYWORD TO DECLARE VARIABLES THAT CANNOT BE MUTATED
