83e1f7e6736b3cfbc894efaf928bb08d41121605
Ehicodes <osaigbovoehinomen@gmail.com>
2025-01-08 03:00:33 +0100

    second commit

--- assignments.js
+++ assignments.js
@@ -10,3 +10,18 @@ let population = 2238000000;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+/* ASSIGNMENT 2 /*Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+ Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console  */
+
+let isIsland = true;
+
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof country);
+console.log(typeof population);
