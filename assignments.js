fafedf41cd475f0379c7a3971243aa4e4c9f2db0
Ehicodes <osaigbovoehinomen@gmail.com>
2025-01-08 02:41:18 +0100

    first assignment

---
+++ assignments.js
@@ -0,0 +1,12 @@
+/*ASSIGNMENT 1 (VALUES AND VARIABLES)  Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions) Log their values to the console */
+
+let country = "Nigeria";
+
+let continent = "Africa";
+
+let population = 2238000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
