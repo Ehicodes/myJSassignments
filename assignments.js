77aec90ccdc7e43e35f3ce3a53e535db90ebbbc8
Ehicodes <osaigbovoehinomen@gmail.com>
2025-01-12 23:54:16 +0100

    assignment 6, if/else statements

--- assignments.js
+++ assignments.js
@@ -1,12 +1,11 @@
 // ASSIGNMENT 1 (VALUES AND VARIABLES)  Declare variables called 'country', 'continent' and 'population' and
 // assign their values according to your own country (population in millions) Log their values to the console
 
-// const country = "Nigeria";
+const country = "Nigeria";
 
 // const continent = "Africa";
 
-// let population = 235203683;
-
+let population = 237;
 // console.log(country);
 // console.log(continent);
 // console.log(population);
@@ -47,8 +46,8 @@ have less people than the average country?
 which contains a string with this format: 'Portugal is in Europe, and its 11 million
 people speak portuguese'
 */
-const finlandPopulatiion = 6000000;
-const averagePopulation = 30000000;
+const finlandPopulatiion = 6;
+const averagePopulation = 33;
 // const description =
 //   country +
 //   " is in " +
@@ -65,8 +64,27 @@ const averagePopulation = 30000000;
 // console.log(population < averagePopulation);
 // console.log(description);
 
-//ASSIGNMENT 5
-// Recreate the 'description' variable from the last assignment, this time
-// using the template literal syntax
-// const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
-// console.log(description);
+/*ASSIGNMENT 5
+ Recreate the 'description' variable from the last assignment, this time
+ using the template literal syntax 
+ */
+//  const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+//  console.log(description);
+
+/* ASSIGNMENT 6
+1.If your country's population is greater that 33 million, log a string like this to the
+console: 'Portugal's population is above average'. Otherwise, log a string like
+'Portugal's population is 22 million below average' (the 22 is the average of 33
+minus the country's population)
+2. After checking the result, change the population temporarily to 13 and then to
+130. See the different results, and set the population back to original */
+
+if (population > 33) {
+  console.log(`${country}'s  population is above average`);
+} else {
+  const belowAverage = averagePopulation - population;
+  console.log(
+    // `${country}'s population is ${33 - population} million below average`
+    `${country}'s population is ${belowAverage} million below average`
+  );
+}
