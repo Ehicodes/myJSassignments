530628d3c3fe98a01193b52ef5b1adddb8f7a893
Ehicodes <osaigbovoehinomen@gmail.com>
2025-01-20 22:19:20 +0100

    challenge 3 in progress

--- codingchallenges.js
+++ codingchallenges.js
@@ -84,26 +84,61 @@ Test data:
 //   console.log("There's a draw!!!");
 // }
 
-//BONUS 1 and BONUS 2
-const teamDolphins = 97 + 112 + 101;
-const teamKoalas = 109 + 95 + 106;
-const teamDolphinsAverage = teamDolphins / 3;
-const teamKoalasAverage = teamKoalas / 3;
-console.log(teamDolphinsAverage, teamKoalasAverage);
+// //BONUS 1 and BONUS 2
+// const teamDolphins = 97 + 112 + 101;
+// const teamKoalas = 109 + 95 + 106;
+// const teamDolphinsAverage = teamDolphins / 3;
+// const teamKoalasAverage = teamKoalas / 3;
+// console.log(teamDolphinsAverage, teamKoalasAverage);
 
-if (teamDolphinsAverage > teamKoalasAverage && teamDolphinsAverage >= 100) {
-  console.log("Dolphins win the trophy🏆");
-} else if (
-  teamKoalasAverage > teamDolphinsAverage &&
-  teamKoalasAverage >= 100
-) {
-  console.log("Koalas wins the trophy 🏆");
-} else if (
-  teamDolphinsAverage === teamKoalasAverage &&
-  teamDolphinsAverage >= 100 &&
-  teamKoalasAverage >= 100
-) {
-  console.log("There's a draw!!!, both team wins the trophy🏆");
+// if (teamDolphinsAverage > teamKoalasAverage && teamDolphinsAverage >= 100) {
+//   console.log("Dolphins win the trophy🏆");
+// } else if (
+//   teamKoalasAverage > teamDolphinsAverage &&
+//   teamKoalasAverage >= 100
+// ) {
+//   console.log("Koalas wins the trophy 🏆");
+// } else if (
+//   teamDolphinsAverage === teamKoalasAverage &&
+//   teamDolphinsAverage >= 100 &&
+//   teamKoalasAverage >= 100
+// ) {
+//   console.log("There's a draw!!!, both team wins the trophy🏆");
+// } else {
+//   console.log("No one wins the trophy😢");
+// }
+
+//CODING CHALLENGE 4
+/*
+Steven wants to build a very simple tip calculator for whenever he goes eating in a
+restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and
+300. If the value is different, the tip is 20%.
+Your tasks:
+1. Calculate the tip, depending on the bill value. Create a variable called 'tip' for
+this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can
+start with an if/else statement, and then try to convert it to a ternary
+operator!)
+2. Print a string to the console containing the bill value, the tip, and the final value
+(bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value
+316.25”
+Test data:
+§ Data 1: Test for bill values 275, 40 and 430
+Hints:
+§ To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+§ Value X is between 50 and 300, if it's >= 50 && <= 300 😉
+*/
+
+billValue = 430;
+tip = 15 / 100;
+const tipAmount = billValue * tip;
+if (billValue >= 50 && billValue <= 300) {
+         console.log(`the bill was ${billValue}` * )
 } else {
-  console.log("No one wins the trophy😢");
+  console.log(`${billValue}` * 0.2);
 }
+
+
+// console.log(
+//     `${billValue}` * 0.15
+//   )`the bill was ${billValue}, the tip was  ${tip}` &&
+//     `the total value ${billValue + tip}`;
